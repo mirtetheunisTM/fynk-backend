d66193429ea06176bd0d73e284b3736e1b57ea7c
atoolate <alexdelaet00@gmail.com>
2025-06-07 22:40:24 +0200

    feat: prepare JWT authentication middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const authController = require('../controllers/auth');
+const authMiddleware = require('../middleware/auth');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
